87e51e9698b21bf72d3e94199cd9e8cf454b517e
Monzerarby20 <monzermahmoud77@gmail.com>
2025-10-02 14:09:14 +0300

    add packg.json and some files

--- src/components/ui/theme-switcher.tsx
+++ src/components/ui/theme-switcher.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from '../ui/button';
+import { Button } from './button';
 import { useTheme } from '../../contexts/ThemeContext';
 import { Moon, Sun, Languages, Globe } from 'lucide-react';
 import { cn } from '../../lib/utils';
